26e9ce6dbd2be081eab8c754eba96552206e49af
HawksleyNem <hawksley.mqt@gmail.com>
2024-02-02 14:25:57 +0100

    fix navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const burgerButton = document.querySelector('.burger-menu');
-    const header = document.querySelector('header');
-    const mobileMenu = document.querySelector('.mobile-menu');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const burgerButton = document.querySelector('.burger-menu');
+//     const header = document.querySelector('header');
+//     const mobileMenu = document.querySelector('.mobile-menu');
 
-    burgerButton.addEventListener('click', function () {
-        header.classList.toggle('header-menu');
-    });
-});
+//     burgerButton.addEventListener('click', function () {
+//         header.classList.toggle('header-menu');
+//         mobileMenu.style.display = "block";
+//     });
+// });
